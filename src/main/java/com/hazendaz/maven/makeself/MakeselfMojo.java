b8ad0a2d4a17eb4073a0f463c7b10b580c5fae2e
Jeremy Landis <jeremylandis@hotmail.com>
2020-12-31 12:43:05 -0500

    [adjust] Use proper case for Path and add debugging

--- src/main/java/com/hazendaz/maven/makeself/MakeselfMojo.java
+++ src/main/java/com/hazendaz/maven/makeself/MakeselfMojo.java
@@ -550,7 +550,8 @@ public class MakeselfMojo extends AbstractMojo {
         // Add portable git to windows environment
         if (MakeselfMojo.WINDOWS) {
             Map<String, String> envs = processBuilder.environment();
-            envs.put("Path", localRepository.getBasedir() + "/PortableGit/usr/bin/");
+            envs.put("PATH", localRepository.getBasedir() + "/PortableGit/usr/bin/" + ";" + envs.get("PATH"));
+            getLog().debug("Environment Variables: " + envs);
         }
 
         // Create Process
