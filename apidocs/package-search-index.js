628ebc5fbb5c532287c178c4d23fa32ee1bbda65
Jeremy Landis <jeremylandis@hotmail.com>
2022-11-10 12:35:03 -0500

    Site checkin for project makeself-maven-plugin

--- apidocs/package-search-index.js
+++ apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.hazendaz.maven.makeself"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.github.hazendaz.maven.makeself_maven_plugin"},{"l":"com.hazendaz.maven.makeself"}];updateSearchResults();
